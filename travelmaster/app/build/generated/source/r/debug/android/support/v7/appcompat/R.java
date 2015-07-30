a693d55c1e4bc27ce256e87c71fe6652ae36ff07
Felix <felixx_schneider@web.de>
2015-07-30 15:20:26 +0200

    gui_androidstudio
    mo gugge obs klappt boys :dancers:

--- travelmaster/app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
+++ travelmaster/app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
@@ -555,18 +555,18 @@ public final class R {
 		public static final int abc_search_view = 0x7f040016;
 		public static final int abc_select_dialog_material = 0x7f040017;
 		public static final int abc_simple_dropdown_hint = 0x7f040018;
-		public static final int notification_media_action = 0x7f04001a;
-		public static final int notification_media_cancel_action = 0x7f04001b;
-		public static final int notification_template_big_media = 0x7f04001c;
-		public static final int notification_template_big_media_narrow = 0x7f04001d;
-		public static final int notification_template_lines = 0x7f04001e;
-		public static final int notification_template_media = 0x7f04001f;
-		public static final int notification_template_part_chronometer = 0x7f040020;
-		public static final int notification_template_part_time = 0x7f040021;
-		public static final int select_dialog_item_material = 0x7f040022;
-		public static final int select_dialog_multichoice_material = 0x7f040023;
-		public static final int select_dialog_singlechoice_material = 0x7f040024;
-		public static final int support_simple_spinner_dropdown_item = 0x7f040025;
+		public static final int notification_media_action = 0x7f04001b;
+		public static final int notification_media_cancel_action = 0x7f04001c;
+		public static final int notification_template_big_media = 0x7f04001d;
+		public static final int notification_template_big_media_narrow = 0x7f04001e;
+		public static final int notification_template_lines = 0x7f04001f;
+		public static final int notification_template_media = 0x7f040020;
+		public static final int notification_template_part_chronometer = 0x7f040021;
+		public static final int notification_template_part_time = 0x7f040022;
+		public static final int select_dialog_item_material = 0x7f040023;
+		public static final int select_dialog_multichoice_material = 0x7f040024;
+		public static final int select_dialog_singlechoice_material = 0x7f040025;
+		public static final int support_simple_spinner_dropdown_item = 0x7f040026;
 	}
 	public static final class string {
 		public static final int abc_action_bar_home_description = 0x7f060000;
